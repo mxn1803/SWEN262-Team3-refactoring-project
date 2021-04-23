b2f3fc0746e3fda3d5a2efe035a4b87c4c9d7069
Mike Nystoriak <mxn1803@rit.edu>
2021-04-23 13:42:48 -0400

    Reduced logic in panel creation.

--- src/main/java/LaneStatusView.java
+++ src/main/java/LaneStatusView.java
@@ -20,12 +20,9 @@ public class LaneStatusView implements ActionListener, LaneObserver, PinsetterOb
     private final PinSetterView PINSETTER_VIEW;
     private final LaneView LANE_VIEW;
     private final Lane LANE;
-    int laneNum;
 
     public LaneStatusView(Lane lane, int laneNum) {
-
         this.LANE = lane;
-        this.laneNum = laneNum;
 
         PINSETTER_VIEW = new PinSetterView(laneNum);
         Pinsetter ps = lane.getPinsetter();
@@ -34,7 +31,6 @@ public class LaneStatusView implements ActionListener, LaneObserver, PinsetterOb
         LANE_VIEW = new LaneView(lane, laneNum);
         lane.subscribe(LANE_VIEW);
 
-
         LANE_PANEL = new JPanel();
         LANE_PANEL.setLayout(new FlowLayout());
         JLabel cLabel = new JLabel("Now Bowling: ");
@@ -42,44 +38,26 @@ public class LaneStatusView implements ActionListener, LaneObserver, PinsetterOb
         JLabel pdLabel = new JLabel("Pins Down: ");
         PINS_DOWN_LABEL = new JLabel("0");
 
-        // Button Panel
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new FlowLayout());
-
         VIEW_LANE_BUTTON = new JButton("View Lane");
-        JPanel viewLanePanel = new JPanel();
-        viewLanePanel.setLayout(new FlowLayout());
         VIEW_LANE_BUTTON.addActionListener(this);
-        viewLanePanel.add(VIEW_LANE_BUTTON);
 
         VIEW_PINSETTER_BUTTON = new JButton("Pinsetter");
-        JPanel viewPinSetterPanel = new JPanel();
-        viewPinSetterPanel.setLayout(new FlowLayout());
         VIEW_PINSETTER_BUTTON.addActionListener(this);
-        viewPinSetterPanel.add(VIEW_PINSETTER_BUTTON);
 
         MAINTENANCE_BUTTON = new JButton("Fix Problem");
         MAINTENANCE_BUTTON.setBackground(Color.GREEN);
-        JPanel maintenancePanel = new JPanel();
-        maintenancePanel.setLayout(new FlowLayout());
         MAINTENANCE_BUTTON.addActionListener(this);
-        maintenancePanel.add(MAINTENANCE_BUTTON);
 
         VIEW_LANE_BUTTON.setEnabled(false);
         VIEW_PINSETTER_BUTTON.setEnabled(false);
-
-
-        buttonPanel.add(viewLanePanel);
-        buttonPanel.add(viewPinSetterPanel);
-        buttonPanel.add(maintenancePanel);
 
         LANE_PANEL.add(cLabel);
         LANE_PANEL.add(CURRENT_BOWLER_LABEL);
         LANE_PANEL.add(pdLabel);
         LANE_PANEL.add(PINS_DOWN_LABEL);
-
-        LANE_PANEL.add(buttonPanel);
-
+        LANE_PANEL.add(VIEW_LANE_BUTTON);
+        LANE_PANEL.add(VIEW_PINSETTER_BUTTON);
+        LANE_PANEL.add(MAINTENANCE_BUTTON);
     }
 
     public JPanel getLanePanel() {
